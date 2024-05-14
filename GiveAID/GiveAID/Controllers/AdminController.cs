e56eb79b446551b9ea4a6ae602eac8b9828679ae
minduke <leduc20198@gmail.com>
2024-05-14 19:34:35 +0700

    news

--- GiveAID/GiveAID/Controllers/AdminController.cs
+++ GiveAID/GiveAID/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GiveAID.Models.entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,7 @@ namespace GiveAID.Controllers
 {
     public class AdminController : BaseController
     {
+        GiveAIDEntities en = new GiveAIDEntities();
         // GET: Admin
         public ActionResult Index()
         {
@@ -16,7 +19,32 @@ namespace GiveAID.Controllers
 
         public ActionResult CreateNews()
         { 
+            ViewBag.category = en.categories.ToList();
             return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public JsonResult SubmitNews(post post, HttpPostedFileBase fileBase)
+        {
+            var PathUpload = Server.MapPath("/Content/Images");
+            if(!Directory.Exists(PathUpload))
+            {
+                Directory.CreateDirectory(PathUpload);
+            }
+
+            string fileExtension = Path.GetExtension(fileBase.FileName).ToLower();
+            if (fileExtension == ".jpg" || fileExtension == ".png" || fileExtension == ".gif")
+            {
+                var fileName = DateTime.Now.Ticks + fileBase.FileName;
+                PathUpload += "\\" + fileName;
+                fileBase.SaveAs(PathUpload);
+                post.image = fileName;
+                en.posts.Add(post);
+                en.SaveChanges();
+                return Json(new {result = true});
+            }
+            return Json(new { result = false, error = "Sai định dạng ảnh" });
         }
 
     }
