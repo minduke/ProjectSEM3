8954a07c34c947ba1b8bb62a8c0ab34da3f4a9be
Nguyên Vũ Lại <vuvunguyen8420@gmail.com>
2024-05-17 08:50:23 +0700

    Update class js css custom

--- GiveAID/GiveAID/Content/custom/customJS.js
+++ GiveAID/GiveAID/Content/custom/customJS.js
@@ -9,16 +9,33 @@ function showErrorMessage(text) {
 }
 
 function handleException(response) {
-    //HayremoveClass("loading");
+    VipremoveClass("loading");
     var error = response.responseJSON;
     showErrorMessage(error.error);
 }
 
 function getFormObj(formId) {
+    //form vip pro 123 456 789
     var formObj = {};
     var inputs = $('#' + formId).serializeArray();
     $.each(inputs, function (i, input) {
         formObj[input.name] = input.value;
     });
     return formObj;
+}
+
+//add Class
+function VipaddClass(className) {
+    //add Class
+    $("body").addClass(className);
+}
+
+//remove Class
+function VipremoveClass(className) {
+    //remove Class
+    $("body").removeClass(className);
+}
+
+function hayAddClass(className) {
+    $("body").addClass(className);
 }
