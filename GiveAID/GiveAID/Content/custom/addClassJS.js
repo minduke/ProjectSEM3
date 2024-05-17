8f9be984bf4633d3905a6493a39578b6820686c7
Nguyên Vũ Lại <vuvunguyen8420@gmail.com>
2024-05-17 10:54:41 +0700

    add Class, sửa layout admin, user

---
+++ GiveAID/GiveAID/Content/custom/addClassJS.js
@@ -0,0 +1,14 @@
+//add Class
+function VipaddClass(className) {
+    //add Class
+    $("body").addClass(className);
+}
+
+//remove Class
+function VipremoveClass(className) {
+    //remove Class
+    $("body").removeClass(className);
+}
+function hayAddClass(className) {
+    $("body").addClass(className);
+}
